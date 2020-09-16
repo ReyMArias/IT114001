030b86b91856ba07ec3947cf869e7b9ef31e4102
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-09-16 19:59:53 -0400

    Added [HW] Java Data

---
+++ Basics/src/Homework.java
@@ -0,0 +1,25 @@
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Create a variable that holds a name
+
+		// 2. Output the name to the console
+
+		// 3. Create a variable that holds an age (as a whole number)
+
+		// 4. Output the age to the console
+
+		// 5. Create a variable that holds a decimal
+
+		// 6. Output the variable
+
+		String name = "Reynaldo M. Arias";
+		int age = 19;
+		double decimal = 3.14;
+
+		System.out.print(name + "\n" + age + "\n" + decimal);
+	}
+
+}
