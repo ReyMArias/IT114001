63fe00aaee441f65a91783d3b2a6a090d2f5b24f
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-09-30 19:45:23 -0400

    Adding Reccusion2Loop

---
+++ Basics/src/Recursion2Loop.java
@@ -0,0 +1,16 @@
+
+public class Recursion2Loop {
+
+	public static void main(String[] args) {
+
+		int count = 0;
+
+		for (int i = 0; i < 11; i++) {
+			count += i;
+		}
+
+		System.out.println(count);
+
+	}
+
+}
